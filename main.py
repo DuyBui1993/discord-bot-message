edaccc926213f69cca90b2395af7b9da61a75f2e
Duy Bui <duy.bui@doltech.vn>
2025-07-15 21:43:41 +0700

    update 2

--- main.py
+++ main.py
@@ -9,21 +9,21 @@ channel_id = int(os.getenv('CHANNEL_ID'))
 print(f"Channel ID: {channel_id}")
 
 class MyClient(discord.Client):
+    def __init__(self):
+        super().__init__()
+        self.message_channel = None
+
     async def on_ready(self):
         print(f'Logged on as {self.user}!')
         await self.do_spam()
 
     async def do_spam(self):
-        channel = self.get_channel(channel_id)
+        self.message_channel = self.get_channel(channel_id)
         while True:
-            await channel.send('/dice amount:5000', delete_after=3)
+            await self.message_channel.send('xin coin', delete_after=1)
             print('Message sent!')
-            await asyncio.sleep(6)
-            await self.close()
-            await asyncio.sleep(2)
-            await self.start(user_token)
+            await asyncio.sleep(1)
 
 
 client = MyClient()
 client.run(user_token)
-
