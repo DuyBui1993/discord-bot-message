b8a2ab99a89f9ca84bb6aa382d917c5a429d083c
Duy Bui <duy.bui@doltech.vn>
2025-07-19 16:17:04 +0700

    fix: [DT-4662] add spam

--- main.py
+++ main.py
@@ -20,9 +20,12 @@ class MyClient(discord.Client):
     async def do_spam(self):
         self.message_channel = self.get_channel(channel_id)
         while True:
-            await self.message_channel.send('/dice amount:5000')
+            await self.message_channel.send('Lạy ông đi qua, lạy bà đi lại, thương tình cho con xin ít coin để sống qua ngày ạ! thành tâm xin coin')
             print('Message sent!')
-            await asyncio.sleep(10)
+            await asyncio.sleep(1)
+            await self.close()
+            await asyncio.sleep(2)
+            await self.start(user_token)
 
 
 client = MyClient()
